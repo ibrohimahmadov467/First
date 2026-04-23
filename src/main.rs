5e4ddd542694b67c1581370137e1c713901c04af
root <root@localhost.localdomain>
2026-04-23 14:41:38 +0000

    Yangi

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use macroquad::prelude::*;
 async fn main() {
     loop {
         clear_background(LIGHTGRAY);
-        
-        draw_rectangle(10.0, 10.0, 300.0, 300.0, Color::from_hex(0x34AD34));
+        let mut x = 1.0;
+        while 100.0 > x{
+            x+= 1.0;
+        }
+        draw_rectangle(x, x, 300.0, 300.0, Color::from_hex(0x34AD34));
         next_frame().await; // BU JUDA MUHIM!
     }
 }
