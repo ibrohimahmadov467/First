21dd6b1ed078ca662d760bb5b2abb403fc5f86eb
root <root@localhost.localdomain>
2026-04-23 15:05:52 +0000

    Harakat

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ async fn main() {
     loop {
         clear_background(LIGHTGRAY);
         let mut x = 1.0;
-        while 100.0 > x{
-            x+= 1.0;
-        }
+        let mut a = true;
+        
+        if x <= 1.0 {a = true;} 
+        else if x >= 100.0 {a = false;}
+
+        if a{x+=1.0;}
+        else{x-=1.0;}
+        
         draw_rectangle(x, x, 300.0, 300.0, Color::from_hex(0x34AD34));
         next_frame().await; // BU JUDA MUHIM!
     }
 }
-
