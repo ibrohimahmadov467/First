1cdeee6bbf2ab1c9df3ce314d2d3f926840a402d
root <root@localhost.localdomain>
2026-04-23 15:07:49 +0000

    Sfgfggg

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use macroquad::prelude::*;
 async fn main() {
     loop {
         clear_background(LIGHTGRAY);
-        let mut x = 1.0;
+        let mut x = 0.0;
         let mut a = true;
         
         if x <= 1.0 {a = true;} 
-        else if x >= 100.0 {a = false;}
+        else if x >= 1000.0 {a = false;}
 
-        if a{x+=1.0;}
-        else{x-=1.0;}
+        if a{x+=10.0;}
+        else{x-=10.0;}
         
         draw_rectangle(x, x, 300.0, 300.0, Color::from_hex(0x34AD34));
         next_frame().await; // BU JUDA MUHIM!
